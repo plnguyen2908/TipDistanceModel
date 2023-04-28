85e65b55668f5a265e520e9d3ba50abdb717acf3
Mạnh <62018096+manhbeo@users.noreply.github.com>
2023-04-28 11:06:21 -0500

    Add files via upload
    process.R file for the linear regression

--- process.R
+++ process.R
@@ -1,29 +1,37 @@
-library(tidyverse)
+args <- commandArgs(trailingOnly = TRUE)
 
-args = commandArgs(trailingOnly=TRUE)
+if (length(args) < 2) {
+  stop("Usage: Rscript process.R train_file test_file\n")
+}
 
-file = paste("yellow_tripdata_",args[1],"-",args[2],".csv", sep="")
-# print(file)
-# data = read.csv("yellow_tripdata_2019-01.csv")
-data = read.csv(file)
+train_data <- read.csv(args[1])     
+test_data <- read.csv(args[2])   
 
-# print(data)
+X_train <- as.matrix(train_data[, 1])
+X_test <- as.matrix(test_data[, 1])
 
 
-data1 = data %>% 
-  filter(trip_distance >= 0 & trip_distance <=20 & tip_amount <= 10 & tip_amount >= 0 & fare_amount >= 0 & fare_amount <= 100) %>%
-  drop_na(passenger_count) %>% 
-  select(
-    passenger_count,
-    trip_distance,
-    tip_amount,
-    fare_amount,
-    extra, 
-    tpep_pickup_datetime,
-  ) %>% 
-  mutate(
-    year = substring(tpep_pickup_datetime,1,4)
-  ) %>% 
-  select(-tpep_pickup_datetime)
-# print(paste(substring(file, 1, nchar(file) - 4), "_new", ".csv", sep = ""))
-write.csv(data1, file = paste(substring(file, 1, nchar(file) - 4), "_new", ".csv", sep = ""))
+y_train <- train_data[, 2]
+y_test <- test_data[, 2]
+
+
+min_cost <- Inf
+best_degree <- -1
+
+
+for (degree in 1:10) {
+  
+  X_train_degree <- poly(X_train, degree, raw = TRUE)
+  X_test_degree <- poly(X_test, degree, raw = TRUE)
+  
+  lm_model <- lm(y_train ~ X_train_degree)
+  
+  cost <- sum((y_test - predict(lm_model, X_test_degree))^2)
+  
+  if (cost < min_cost) {
+    min_cost <- cost
+    best_degree <- degree
+  }
+}
+
+write.table(best_degree, file = paste(sep="", args[1], "_result"), row.names = FALSE, col.names = FALSE)
